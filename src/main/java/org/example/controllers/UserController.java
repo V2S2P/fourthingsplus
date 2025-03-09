d52e0df64a63810fb13a3dcb6269e7b882fee7b5
V2S2P <157969468+V2S2P@users.noreply.github.com>
2025-03-09 23:36:40 +0100

    Finished video 9 create user

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -14,6 +14,32 @@ import java.util.List;
 public class UserController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool){
         app.post("login",ctx -> login(ctx, connectionPool));
+        app.get("logout",ctx -> logout(ctx));
+        app.get("createuser", ctx -> ctx.render("createuser.html"));
+        app.post("createuser", ctx -> createUser(ctx, connectionPool));
+    }
+    private static void createUser(Context ctx, ConnectionPool connectionPool){
+        String username = ctx.formParam("username");
+        String password1 = ctx.formParam("password1");
+        String password2 = ctx.formParam("password2");
+
+        if (password1.equals(password2)){
+            try {
+                UserMapper.createuser(username, password1, connectionPool);
+                ctx.attribute("message", "Congratulations, you have created an account!");
+                ctx.render("index.html");
+            } catch (DatabaseException e) {
+                ctx.attribute("message", "Username already exists. Try again.");
+                ctx.render("createuser.html");
+            }
+        }else {
+            ctx.attribute("message", "Your passwords don't match");
+            ctx.render("createuser.html");
+        }
+    }
+    public static void logout(Context ctx){
+        ctx.req().getSession().invalidate();
+        ctx.redirect("/");
     }
     public static void login(Context ctx, ConnectionPool connectionPool){
         String username = ctx.formParam("username");
